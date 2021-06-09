2bb89f184bbc919e3111395944523c2358b0899d
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-06-09 17:33:29 +0300

    c stateful component

--- Part1/material/src/App.js
+++ Part1/material/src/App.js
@@ -1,35 +1,17 @@
-import React from 'react'
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
-
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-  
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter+1),
+    2000
+  )
+
+  console.log('rendering...', counter)
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
+    <div>{counter}</div>
   )
 }
 
