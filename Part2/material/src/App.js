7a3e1e57a5b7a560ad01caddba2abb07cd5c43f5
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-07-05 19:04:57 +0300

    b forms

--- Part2/material/src/App.js
+++ Part2/material/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) =>
+const App = (props) =>
 {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const handleNoteChange = (event) =>
+  {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) =>
+  {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note =>
-          <Note key={note.id} note={note}/>  // Here the key is the attr of Note: element in the array
+          <Note key={note.id} note={note} />  // Here the key is the attr of Note: element in the array
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
