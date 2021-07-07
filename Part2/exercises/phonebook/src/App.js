83d1c49e9d8554da760b76ecb5fee0e65f4ddc0d
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-07-07 17:32:29 +0300

    exercise 2.9

--- Part2/exercises/phonebook/src/App.js
+++ Part2/exercises/phonebook/src/App.js
@@ -3,20 +3,37 @@ import React, { useState } from 'react'
 const App = () =>
 {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0401234567' }
-  ])
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    { name: 'Kimi Räikkönen', number: '0465656565' }
+  ]);
   const [newName, setNewName] = useState('');
-  const [newNumber,setNewNumber] = useState('');
-  
+  const [newNumber, setNewNumber] = useState('');
+  const [filterWord, setFilterWord] = useState('');
+
+  const filterWordChange = (event) =>
+  {
+    setFilterWord(event.target.value);
+  }
+
   const nameChange = (event) =>
   {
     setNewName(event.target.value);
   };
-  
+
   const numberChange = (event) =>
   {
     setNewNumber(event.target.value);
   };
+
+  const insensitiveInclude = (psn) =>
+  {
+    return psn.name.toLowerCase().includes(filterWord.toLowerCase());
+  }
+
+  const filtered = filterWord ? persons.filter(insensitiveInclude) : persons; // to reduce consumption
 
   const formSubmit = (event) =>
   {
@@ -42,14 +59,16 @@ const App = () =>
   return (
     <div>
       <h2>Phonebook</h2>
+      <div><span>filter shown with </span><input value={filterWord} onChange={filterWordChange} /></div>
+      <h2>add a new</h2>
       <form onSubmit={formSubmit}>
         <div>name: <input value={newName} onChange={nameChange} /></div>
-        <div>number: <input value={newNumber} onChange={numberChange}/></div>
+        <div>number: <input value={newNumber} onChange={numberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(psn => <div key={psn.name}>{psn.name} {psn.number}</div>)}
+        {filtered.map(psn => <div key={psn.name}>{psn.name} {psn.number}</div>)}
       </div>
     </div>
   )
