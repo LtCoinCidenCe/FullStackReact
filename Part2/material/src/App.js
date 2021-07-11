1c716732bf50728f6cc2d87f2771cf11b9fdbb10
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-07-11 13:11:25 +0300

    b effect-hooks

--- Part2/material/src/App.js
+++ Part2/material/src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) =>
+const App = () =>
 {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () =>
+  {
+    console.log('effect')
+
+    const eventHandler = (response) =>
+    {
+      console.log('promise fulfilled');
+      setNotes(response.data);
+    }
+
+    const promise = axios.get('http://localhost:3001/notes');
+    promise.then(eventHandler);
+  }
+
+  useEffect(hook, [])
+
 
   const handleNoteChange = (event) =>
   {
